9eb768a02c92204362d1dec2db850200bb340559
Lia Va <mistickusya.2012@gmail.com>
2022-04-13 20:41:36 +0200

    feat: add mil unit

---
+++ apps/military_unit/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MilitaryUnitConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'military_unit'
---
+++ apps/military_unit/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+from apps.general.models import MilitaryRank, MilitarySpecialization, Position, TariffGrid
+
+
+class MilitaryUnit(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    street = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Staff(models.Model):
+    military_unit = models.ForeignKey(
+        MilitaryUnit,
+        on_delete=models.CASCADE,
+        related_name='staff'
+    )
+    tarif = models.ForeignKey(
+        TariffGrid,
+        on_delete=models.CASCADE,
+        related_name='military_unit_staff'
+    )
+    military_specialization = models.ForeignKey(
+        MilitarySpecialization,
+        on_delete=models.CASCADE,
+        related_name='military_unit_staff'
+    )
+    staff_military_rank = models.ForeignKey(
+        MilitaryRank,
+        on_delete=models.CASCADE,
+        related_name='military_unit_staff'
+    )
+    military_rank = models.ForeignKey(
+        MilitaryRank,
+        on_delete=models.CASCADE,
+        related_name='military_unit_staff'
+    )
+    full_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.full_name} ({self.staff_military_rank}) з {self.military_unit.name}"
