a8dc61fca4dfb919ae591578d3e7d9ce96575c94
Lia Va <mistickusya.2012@gmail.com>
2022-04-22 20:08:07 +0200

    fix: street address

--- apps/common/models.py
+++ apps/common/models.py
@@ -33,4 +33,4 @@ class StreetAddress(UUIDModel):
     name = models.CharField(max_length=255)
 
     def __str__(self):
-        return f'{self.city}, {self.street}'
+        return f'{self.city}, {self.name}'
