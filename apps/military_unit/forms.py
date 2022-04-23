9c48b1b45eadc2c25e94b094faf33c8090631499
Lia Va <mistickusya.2012@gmail.com>
2022-04-23 11:18:07 +0200

    fix: normal address

--- apps/military_unit/forms.py
+++ apps/military_unit/forms.py
@@ -1,8 +1,29 @@
 from django import forms
+from apps.common.models import City
 from apps.military_unit.models import Person
 
 
 class PersonForm(forms.ModelForm):
+    city = forms.ModelChoiceField(City.objects.all(), required=True)
+    address = forms.CharField(max_length=255)
+
     class Meta:
         model = Person
-        fields = '__all__'
+        fields = (
+            'first_name',
+            'last_name',
+            'middle_name',
+            'gender',
+            'birth_date',
+            'city',
+            'address',
+            'family_status',
+            'children_number',
+            'phone_number',
+            'tin',
+            'recruitment_office',
+            'military_rank',
+            'military_specialization',
+            'length_of_service',
+            'contract_term',
+        )
