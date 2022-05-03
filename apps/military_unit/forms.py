fd10074ec93c9bbb85196eae7d763109a02c4dbf
RomariY <yaremakroma@gmail.com>
2022-05-03 17:25:00 +0300

    Edit form

--- apps/military_unit/forms.py
+++ apps/military_unit/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-from apps.common.models import City
-from apps.military_unit.models import Person
+from apps.common.models import City, StreetAddress
+from apps.military_unit.models import Person, MilitaryUnit
+
+
+class AddressForm(forms.ModelForm):
+    city = forms.ModelChoiceField(City.objects.all(), required=True, label=_('City'))
+
+    class Meta:
+        model = StreetAddress
+        fields = "__all__"
 
 
 class PersonForm(forms.ModelForm):
@@ -19,6 +27,7 @@ class PersonForm(forms.ModelForm):
             'birth_date',
             'city',
             'address',
+            'position',
             'family_status',
             'children_number',
             'phone_number',
@@ -29,3 +38,35 @@ class PersonForm(forms.ModelForm):
             'length_of_service',
             'contract_term',
         )
+
+    def clean(self):
+        data = super(PersonForm, self).clean()
+        street_address = StreetAddress.objects.create(
+            city=data.get("city"),
+            name=data.get("address"),
+        )
+        data["address"] = street_address
+        return data
+
+
+class MilitaryUnitForm(forms.ModelForm):
+    city = forms.ModelChoiceField(City.objects.all(), required=True, label=_('City'))
+    address = forms.CharField(max_length=255, required=True, label=_('Address'))
+
+    class Meta:
+        model = MilitaryUnit
+        fields = (
+            'name',
+            'military_number',
+            'city',
+            'address',
+        )
+
+    def clean(self):
+        data = super(MilitaryUnitForm, self).clean()
+        street_address = StreetAddress.objects.create(
+            city=data.get("city"),
+            name=data.get("address"),
+        )
+        data["address"] = street_address
+        return data
--- apps/military_unit/views.py
+++ apps/military_unit/views.py
@@ -1,13 +1,23 @@
-#form view
-from django.shortcuts import render
+# form view
 from django.views import generic
 
-from apps.military_unit.models import Person
-from apps.military_unit.forms import PersonForm
+from apps.military_unit.forms import PersonForm, MilitaryUnitForm
+from apps.military_unit.models import Person, MilitaryUnit
 
 
-# Create your views here.
-class MilitaryUnitView(generic.FormView):
+class SuccessPage(generic.TemplateView):
+    template_name = 'military_unit/success.html'
+
+
+class PersonView(generic.CreateView):
     model = Person
     form_class = PersonForm
+    success_url = "/success/"
+    template_name = 'military_unit/person_create.html'
+
+
+class MilitaryUnitView(generic.CreateView):
+    model = MilitaryUnit
+    form_class = MilitaryUnitForm
+    success_url = "/success/"
     template_name = 'military_unit/military_unit_create.html'
