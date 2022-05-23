c8dfecd03cefc10023f786b2b25fb87299231597
vladyslav_belous <vladyslav.belous@gowombat.team>
2022-05-23 09:42:40 +0300

    correct full name for person

--- apps/military_unit/models.py
+++ apps/military_unit/models.py
@@ -130,7 +130,7 @@ class Person(UUIDModel):
 
     @property
     def full_name(self):
-        return f'{self.last_name} {self.middle_name} {self.first_name}'
+        return f'{self.last_name} {self.first_name} {self.middle_name}'
 
     def __str__(self):
         return f'{self.full_name} ({self.military_rank})'
